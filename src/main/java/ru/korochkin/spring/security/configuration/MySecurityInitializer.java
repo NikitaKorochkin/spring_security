307abe11c94737b2fd439ee4a218c38249613565
NikitaKorochkin <nikitakorochkin1997@gmail.com>
2021-04-08 20:09:04 +0300

    Initial commit

---
+++ src/main/java/ru/korochkin/spring/security/configuration/MySecurityInitializer.java
@@ -0,0 +1,7 @@
+package ru.korochkin.spring.security.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+
+public class MySecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
